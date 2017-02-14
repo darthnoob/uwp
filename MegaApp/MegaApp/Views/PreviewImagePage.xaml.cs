f40b4a42368676ea9a5de2dd2d75c73c3bef05ea
Javier Gómez <jg@mega.co.nz>
2017-02-14 17:06:59 +0100

    Zoom for previews

--- MegaApp/MegaApp/Views/PreviewImagePage.xaml.cs
+++ MegaApp/MegaApp/Views/PreviewImagePage.xaml.cs
@@ -1,8 +1,6 @@
-﻿using Windows.System;
-using Windows.UI.Core;
+﻿using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Microsoft.Toolkit.Uwp.UI;
 using MegaApp.Enums;
@@ -71,20 +69,17 @@ namespace MegaApp.Views
             this.PreviousButton.IsEnabled = (itemIndex > 0) ? true : false;
             this.NextButton.IsEnabled = (itemIndex < numItems - 1) ? true : false;
 
-            // Reset scale and position of the old selected image
+            // Reset scale of the old selected image
             foreach (var previousImage in e.RemovedItems)
             {
                 if (previousImage != null)
                 {
                     var container = this.FlipView.ContainerFromItem(previousImage);
-                    if(container != null)
+                    if (container != null)
                     {
-                        var image = container.FindDescendant<Image>();
-                        if (image != null)
-                        {
-                            (image.RenderTransform as CompositeTransform).ScaleX = 1;
-                            (image.RenderTransform as CompositeTransform).ScaleY = 1;
-                        }
+                        var scrollViewer = container.FindDescendant<ScrollViewer>();
+                        if (scrollViewer != null)
+                            scrollViewer.ChangeView(null, null, 1);
                     }
                 }
             }
@@ -92,68 +87,21 @@ namespace MegaApp.Views
 
         private void OnImageDoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
         {
-            var imagePreview = sender as Image;
-            var imagePreviewTransform = imagePreview.RenderTransform as CompositeTransform;
+            var image = sender as Image;
+            if(image != null)
+            {
+                var hOffset = e.GetPosition(image).X;
+                var vOffset = e.GetPosition(image).Y;
 
-            if (imagePreviewTransform.ScaleX > 1 || imagePreviewTransform.ScaleY > 1)
-            {
-                imagePreviewTransform.ScaleX = imagePreviewTransform.ScaleY = 1;
-            }
-            else
-            {
-                imagePreviewTransform.ScaleX = imagePreviewTransform.ScaleY = 2;
-                imagePreviewTransform.CenterX = e.GetPosition(imagePreview).X;
-                imagePreviewTransform.CenterY = e.GetPosition(imagePreview).Y;
-            }
-        }
-
-        private void OnImagePointerWheelChanged(object sender, PointerRoutedEventArgs e)
-        {
-            // If is a Desktop device and the Control key is pressed, zoom in/out the image
-            if (DeviceService.GetDeviceType() == DeviceFormFactorType.Desktop)
-            {
-                var state = CoreWindow.GetForCurrentThread().GetKeyState(VirtualKey.Control);
-                if ((state & CoreVirtualKeyStates.Down) == CoreVirtualKeyStates.Down)
+                var scrollViewer = image.FindAscendant<ScrollViewer>();
+                if(scrollViewer != null)
                 {
-                    var imagePreview = sender as Image;
-                    var imagePreviewTransform = imagePreview.RenderTransform as CompositeTransform;
-
-                    double deltaScroll = e.GetCurrentPoint(imagePreview).Properties.MouseWheelDelta;
-                    deltaScroll = (deltaScroll > 0) ? 1.2 : 0.8;
-
-                    double new_ScaleX = imagePreviewTransform.ScaleX * deltaScroll;
-                    double new_ScaleY = imagePreviewTransform.ScaleY * deltaScroll;
-
-                    imagePreviewTransform.CenterX = e.GetCurrentPoint(imagePreview).Position.X;
-                    imagePreviewTransform.CenterY = e.GetCurrentPoint(imagePreview).Position.Y;
-
-                    imagePreviewTransform.ScaleX = (new_ScaleX > 1) ? new_ScaleX : 1;
-                    imagePreviewTransform.ScaleY = (new_ScaleY > 1) ? new_ScaleY : 1;
-
-                    e.Handled = true;
+                    if (scrollViewer.ZoomFactor > 1)
+                        scrollViewer.ChangeView(hOffset, vOffset, 1);
+                    else
+                        scrollViewer.ChangeView(hOffset, vOffset, 2);
                 }
             }
-        }
-
-        private void OnImageManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
-        {
-            (sender as Image).Opacity = 0.4;
-        }
-
-        private void OnImageManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
-        {
-            var imagePreview = sender as Image;
-            var imagePreviewTransform = imagePreview.RenderTransform as CompositeTransform;
-
-            imagePreviewTransform.TranslateX += e.Delta.Translation.X;
-            imagePreviewTransform.TranslateY += e.Delta.Translation.Y;
-
-            e.Handled = true;
-        }
-
-        private void OnImageManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
-        {
-            (sender as Image).Opacity = 1;
         }
     }
 }
