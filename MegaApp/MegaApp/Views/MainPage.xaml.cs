eb60affa619ffba6315bc9522df6f89ee0e0c292
Javier Gomez <jg@mega.co.nz>
2018-05-17 12:21:02 +0200

    Avoid a possible `NullReferenceException` at `OnNetworkStatusChanged`

--- MegaApp/MegaApp/Views/MainPage.xaml.cs
+++ MegaApp/MegaApp/Views/MainPage.xaml.cs
@@ -153,7 +153,7 @@ namespace MegaApp.Views
         protected override async void OnNetworkStatusChanged(object sender)
         {
             base.OnNetworkStatusChanged(sender);
-            this.ViewModel.ContentViewModel.UpdateNetworkStatus();
+            this.ViewModel?.ContentViewModel?.UpdateNetworkStatus();
 
             // If no network connection, nothing to do
             if (!await NetworkService.IsNetworkAvailableAsync()) return;
@@ -166,7 +166,7 @@ namespace MegaApp.Views
             {
                 UiService.OnUiThread(async () =>
                 {
-                    await this.ViewModel.FastLoginAsync();
+                    if (!await this.ViewModel?.FastLoginAsync()) return;
 
                     if (this.ViewModel?.ContentViewModel is CloudDriveViewModel)
                     {
