872e781fbeb5f99a5bd9a466fa1d845048ef4661
Javier Gomez <jg@mega.co.nz>
2018-07-18 10:12:14 +0200

    Allow export the Recovery Key just after enable the MFA

--- MegaApp/MegaApp/ViewModels/Dialogs/MultiFactorAuthEnabledDialogViewModel.cs
+++ MegaApp/MegaApp/ViewModels/Dialogs/MultiFactorAuthEnabledDialogViewModel.cs
@@ -1,10 +1,46 @@
 ﻿using System;
+using System.Windows.Input;
+using MegaApp.Classes;
 using MegaApp.Services;
 
 namespace MegaApp.ViewModels.Dialogs
 {
     public class MultiFactorAuthEnabledDialogViewModel : BaseContentDialogViewModel
     {
+        public MultiFactorAuthEnabledDialogViewModel() : base()
+        {
+            this.SaveKeyButtonCommand = new RelayCommand(SaveKey);
+        }
+
+        #region Commands
+
+        /// <summary>
+        /// Command invoked when the user select the "Backup Recovery key" option
+        /// </summary>
+        public ICommand SaveKeyButtonCommand { get; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Backup the Recovery key
+        /// </summary>
+        private async void SaveKey()
+        {
+            var saveKeyCommand = SettingsService.RecoveryKeySetting.SaveKeyCommand as RelayCommandAsync<bool>;
+            if (saveKeyCommand == null) return;
+
+            if (!saveKeyCommand.CanExecute(null)) return;
+            var recoveryKeySaved = await saveKeyCommand.ExecuteAsync(null);
+            if (!recoveryKeySaved) return;
+
+            // If the recovery key has been successfully saved close the dialog
+            this.OnHideDialog();
+        }
+
+        #endregion
+
         #region Properties
 
         /// <summary>
@@ -15,10 +51,17 @@ namespace MegaApp.ViewModels.Dialogs
 
         #endregion
 
-        #region AppMessageResources
+        #region AppMessages
 
         public string TitleText => ResourceService.AppMessages.GetString("AM_2FA_EnabledDialogTitle");
         public string DescriptionText => ResourceService.AppMessages.GetString("AM_2FA_EnabledDialogDescription");
+        public string RecommendationText => ResourceService.AppMessages.GetString("AM_2FA_EnabledDialogRecommendation");
+
+        #endregion
+
+        #region UiResources
+
+        public string ExportText => ResourceService.UiResources.GetString("UI_Export");
 
         #endregion
     }
