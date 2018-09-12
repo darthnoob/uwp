ac092e48bdee426de7d103b4dbffafe89e9b9f05
Javier Gomez <jg@mega.co.nz>
2018-09-12 12:58:55 +0200

    Remove an unnecessary `using` directive

--- MegaApp/MegaApp/ViewModels/CameraUploadsViewModel.cs
+++ MegaApp/MegaApp/ViewModels/CameraUploadsViewModel.cs
@@ -3,7 +3,6 @@ using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Globalization;
 using System.Linq;
-using Windows.ApplicationModel.Background;
 using mega;
 using MegaApp.Enums;
 using MegaApp.Interfaces;
